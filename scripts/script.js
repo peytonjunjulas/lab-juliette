529742e4783d04a9ae61aae1a76bae9b7716e583
peytonjunjulas <97271349+peytonjunjulas@users.noreply.github.com>
2022-04-28 00:28:59 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,257 @@
+
+$("#stateSelect").hide();
+
+
+$("#selectStateButton").click(function(){
+  $("#stateSelect").show();
+});
+
+$("#alabama").click(function(){
+  $("#improvedState").val("AL");
+  $("#stateSelect").hide();
+});
+
+$("#alaska").click(function(){
+  $("#improvedState").val("AK");
+  $("#stateSelect").hide();
+});
+
+$("#arizona").click(function(){
+  $("#improvedState").val("AZ");
+  $("#stateSelect").hide();
+});
+
+$("#arkansas").click(function(){
+  $("#improvedState").val("AR");
+  $("#stateSelect").hide();
+});
+
+$("#california").click(function(){
+  $("#improvedState").val("CA");
+  $("#stateSelect").hide();
+});
+
+$("#colorado").click(function(){
+  $("#improvedState").val("CO");
+  $("#stateSelect").hide();
+});
+
+$("#connecticut").click(function(){
+  $("#improvedState").val("CT");
+  $("#stateSelect").hide();
+});
+
+$("#delaware").click(function(){
+  $("#improvedState").val("DE");
+  $("#stateSelect").hide();
+});
+
+$("#florida").click(function(){
+  $("#improvedState").val("FL");
+  $("#stateSelect").hide();
+});
+
+$("#georgia").click(function(){
+  $("#improvedState").val("GA");
+  $("#stateSelect").hide();
+});
+
+$("#hawaii").click(function(){
+  $("#improvedState").val("HI");
+  $("#stateSelect").hide();
+});
+
+$("#idaho").click(function(){
+  $("#improvedState").val("ID");
+  $("#stateSelect").hide();
+});
+
+$("#illinois").click(function(){
+  $("#improvedState").val("IL");
+  $("#stateSelect").hide();
+});
+
+$("#indiana").click(function(){
+  $("#improvedState").val("IN");
+  $("#stateSelect").hide();
+});
+
+$("#iowa").click(function(){
+  $("#improvedState").val("IA");
+  $("#stateSelect").hide();
+});
+
+$("#kansas").click(function(){
+  $("#improvedState").val("KS");
+  $("#stateSelect").hide();
+});
+
+$("#kentucky").click(function(){
+  $("#improvedState").val("KY");
+  $("#stateSelect").hide();
+});
+
+$("#louisiana").click(function(){
+  $("#improvedState").val("LA");
+  $("#stateSelect").hide();
+});
+
+$("#maine").click(function(){
+  $("#improvedState").val("ME");
+  $("#stateSelect").hide();
+});
+
+$("#maryland").click(function(){
+  $("#improvedState").val("MD");
+  $("#stateSelect").hide();
+});
+
+$("#massachusetts").click(function(){
+  $("#improvedState").val("MA");
+  $("#stateSelect").hide();
+});
+
+$("#michigan").click(function(){
+  $("#improvedState").val("MI");
+  $("#stateSelect").hide();
+});
+
+$("#minnesota").click(function(){
+  $("#improvedState").val("MN");
+  $("#stateSelect").hide();
+});
+
+$("#mississippi").click(function(){
+  $("#improvedState").val("MS");
+  $("#stateSelect").hide();
+});
+
+$("#missouri").click(function(){
+  $("#improvedState").val("MO");
+  $("#stateSelect").hide();
+});
+
+$("#montana").click(function(){
+  $("#improvedState").val("MT");
+  $("#stateSelect").hide();
+});
+
+$("#nebraska").click(function(){
+  $("#improvedState").val("NE");
+  $("#stateSelect").hide();
+});
+
+$("#nevada").click(function(){
+  $("#improvedState").val("NV");
+  $("#stateSelect").hide();
+});
+
+$("#newHampshire").click(function(){
+  $("#improvedState").val("NH");
+  $("#stateSelect").hide();
+});
+
+$("#newJersey").click(function(){
+  $("#improvedState").val("NJ");
+  $("#stateSelect").hide();
+});
+
+$("#newMexico").click(function(){
+  $("#improvedState").val("NM");
+  $("#stateSelect").hide();
+});
+
+$("#newYork").click(function(){
+  $("#improvedState").val("NY");
+  $("#stateSelect").hide();
+});
+
+$("#northCarolina").click(function(){
+  $("#improvedState").val("NC");
+  $("#stateSelect").hide();
+});
+
+$("#northDakota").click(function(){
+  $("#improvedState").val("ND");
+  $("#stateSelect").hide();
+});
+
+$("#ohio").click(function(){
+  $("#improvedState").val("OH");
+  $("#stateSelect").hide();
+});
+
+$("#oklahoma").click(function(){
+  $("#improvedState").val("OK");
+  $("#stateSelect").hide();
+});
+
+$("#oregon").click(function(){
+  $("#improvedState").val("OR");
+  $("#stateSelect").hide();
+});
+
+$("#pennsylvania").click(function(){
+  $("#improvedState").val("PA");
+  $("#stateSelect").hide();
+});
+
+$("#rhodeIsland").click(function(){
+  $("#improvedState").val("RI");
+  $("#stateSelect").hide();
+});
+
+$("#southCarolina").click(function(){
+  $("#improvedState").val("SC");
+  $("#stateSelect").hide();
+});
+
+$("#southDakota").click(function(){
+  $("#improvedState").val("SD");
+  $("#stateSelect").hide();
+});
+
+$("#tennessee").click(function(){
+  $("#improvedState").val("TN");
+  $("#stateSelect").hide();
+});
+
+$("#texas").click(function(){
+  $("#improvedState").val("TX");
+  $("#stateSelect").hide();
+});
+
+$("#utah").click(function(){
+  $("#improvedState").val("UT");
+  $("#stateSelect").hide();
+});
+
+$("#vermont").click(function(){
+  $("#improvedState").val("VT");
+  $("#stateSelect").hide();
+});
+
+$("#virginia").click(function(){
+  $("#improvedState").val("VA");
+  $("#stateSelect").hide();
+});
+
+$("#washington").click(function(){
+  $("#improvedState").val("WA");
+  $("#stateSelect").hide();
+});
+
+$("#westVirginia").click(function(){
+  $("#improvedState").val("WV");
+  $("#stateSelect").hide();
+});
+
+$("#wisconsin").click(function(){
+  $("#improvedState").val("WI");
+  $("#stateSelect").hide();
+});
+
+$("#wyoming").click(function(){
+  $("#improvedState").val("WY");
+  $("#stateSelect").hide();
+});
